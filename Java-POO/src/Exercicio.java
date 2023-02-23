ebe1ab3ea2242935a6802f5e60e4665d739c29d5
guilhermeo97 <oliveiraguilherme591@gmail.com>
2023-02-22 22:07:40 -0300

    continuação
    ...

--- Java-POO/src/Exercicio.java
+++ Java-POO/src/Exercicio.java
@@ -1,14 +1,15 @@
 public class Exercicio {
     public static void main(String[] args) throws Exception {
-        Cachorro Dalila = new Cachorro("Vira lata", 40.0f, 2020, "Dalila", 'F', 15.0f, true);
+        Cachorro2 Dalila = new Cachorro2("Vira lata", 40.0f, 2020, "Dalila", 'F', 15.0f, true);
+        Cachorro2 lion = new Cachorro2(50.0f, 2010, "Lion", 'M', 20);
         Dalila.description();
-        Cachorro lion = new Cachorro(50.0f, 2010, "Lion", 'M', 20);
+        Dalila.calculaIMCC(10.0f, 15.0f);
         lion.description();
         System.out.println("Execução Terminada");
     }
 }
 
-class Cachorro {
+class Cachorro2 {
     
     private String race;
     private float size;
@@ -17,8 +18,9 @@ class Cachorro {
     private char sex;
     private float weight;
     private boolean hasVacine;
+    private float IMCC = weight / (size * size);
 
-    public Cachorro(float size,int age, String name, char sex, float weight){
+    public Cachorro2(float size,int age, String name, char sex, float weight){
         this.name = name;
         this.race = "Vira-lata";;
         this.size = size;
@@ -27,7 +29,7 @@ class Cachorro {
         this.weight = weight;
     }
 
-    public Cachorro(String race, float size, int age, String name, char sex, float weight, boolean hasVacine){
+    public Cachorro2(String race, float size, int age, String name, char sex, float weight, boolean hasVacine){
         this.name = name;
         this.race = race;
         this.sex = sex;
@@ -44,5 +46,25 @@ class Cachorro {
         " peso " + this.weight + "ele tem"+ this.age + "anos." +      
         " altura de: " + this.size + 
         " peso " + this.weight + " Vacinada = " + this.hasVacine);
+    }
+
+    public void calculaIMCC(float size, float weight) {
+        this.size = size;
+        this.weight = weight;
+        if(IMCC <= 18.5 ){
+            System.out.println("Está abaixo do peso");
+        } else {
+            if(IMCC > 18.5 && IMCC <= 25){
+                System.out.println("Peso normal");
+            }
+            else {
+                if(IMCC > 25 && IMCC <= 30) {
+                    System.out.println("Acima do peso");
+                }
+                else {
+                    System.out.println("Obeso");
+                }
+            } 
+        }
     }
 }
